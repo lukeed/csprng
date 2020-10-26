ecfd4a31b7d1e6385b699209087dae194f92a786
Luke Edwards <luke.edwards05@gmail.com>
2020-10-26 16:24:23 -0700

    chore: stub `browser` impl for tests

--- test/browser.js
+++ test/browser.js
@@ -1,9 +1,24 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
+import { randomFillSync } from 'crypto';
 import { random } from '../src/browser';
+
+// @ts-ignore
+global.crypto = {
+	getRandomValues(arr) {
+		return randomFillSync(arr);
+	}
+}
 
 test('exports', () => {
 	assert.type(random, 'function');
+});
+
+test('returns', () => {
+	let foo = random(12);
+	assert.instance(foo, Uint8Array);
+	assert.is(foo.byteLength, 12);
+	assert.is(foo.length, 12);
 });
 
 test.run();
